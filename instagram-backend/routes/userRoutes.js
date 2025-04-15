0b2bccb57b6f8919eaf0984f9b379f70bd8c03a9
tsyaputri <127934024+tsyaputri@users.noreply.github.com>
2025-04-16 00:06:50 +0700

    Update userRoutes.js

--- instagram-backend/routes/userRoutes.js
+++ instagram-backend/routes/userRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
+router.get('/', auth, userController.getAllUsers);
 router.get('/:id', auth, userController.getUserProfile);
+router.put('/:id', auth, userController.updateUser);
+router.put('/:id/password', auth, userController.updatePassword);
+router.delete('/:id', auth, userController.deleteUser);
+router.post('/admin/create', auth, userController.adminCreateUser);
 
 module.exports = router;
